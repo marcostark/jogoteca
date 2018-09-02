7b730dff779c1c137158590ab0179dc3dc47dacc
Marcos Stark <mrcs.stark@gmail.com>
2018-09-01 22:36:58 -0300

    Melhoria nas funcionalidades

--- jogoteca.py
+++ jogoteca.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 from jogo import Jogo
+from usuario import Usuario
 
 app = Flask(__name__)
+app.secret_key = 'qgdostark'
+
+user1 = Usuario("stark","Marcos","1234")
+user2 = Usuario("paty","Patricia","4321")
+user3 = Usuario("admin","admin","admin")
+
+usuarios = {user1.get_id(): user1,
+            user2.get_id(): user2,
+            user3.get_id(): user3 }
 
 jogo1 = Jogo("Super Mario","Ação","SNES")
 jogo2 = Jogo("Pokemon GO!","RPG", "Mobile")
@@ -15,6 +25,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima= url_for('novo')))
     return render_template('novo.html',titulo='Novo Jogo')
 
 @app.route('/criar',methods=['POST'])
@@ -26,7 +38,34 @@ def criar():
 
     jogo = Jogo(nome, categoria, plataforma)
     lista_jogos.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima') # pegando argumentos passados pela requisição
+    return render_template('/login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+
+        if usuario.get_senha() == request.form['senha']:
+            session['usuario_logado'] = usuario.get_id()
+            flash(usuario.get_nome() + ' logou com sucesso')
+            proxima_pagina = request.form['proxima']
+            # Se eu não vim de nenhuma pagina antes de login, eu devo ir para a pagina especifica
+            return redirect(proxima_pagina)
+    else:
+        flash('Não logado, tente novamente!')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect(url_for('index'))
 
 # Definindo acesso externo e definido porta 8080
 # Debug=True, detecta as alterações automaticamente, para não precisar restartar a aplicação
